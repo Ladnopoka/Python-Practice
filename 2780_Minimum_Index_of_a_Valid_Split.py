8df48f83119a8f9eaad5b7a8fa1df75cdeeea562
Jevgenij Ivanov <ladnopokaa@gmail.com>
2025-03-27 12:19:50 +0000

    working on leetcode 2780

---
+++ 2780_Minimum_Index_of_a_Valid_Split.py
@@ -0,0 +1,12 @@
+def minimumIndex(arr):
+    print("write your code here")
+
+def main():
+    arr = [1,2,2,2]
+    arr2 = [2,1,3,1,1,1,7,1,2,1]
+    
+    result = minimumIndex(arr2)
+    print(result)
+ 
+if __name__ == "__main__":
+    main()
